d52d5638dd9470df5ebc1295e34deed575a193b7
noname <noname@nonameui-MacBookPro.local>
2025-09-01 21:53:15 +0900

    macos.sh

---
+++ macos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+### Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+### IDEs
+brew install --cask visual-studio-code
+#brew install --cask cursor
+
+### DB and Tools
+brew install --cask dbeaver-community
+
+### Docker and Tools
+brew install --cask docker-desktop
+
